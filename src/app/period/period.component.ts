73f9facb1abb8d4250665275a369420e27982440
Philipp Bomers <philipp.a.bomers@capgemini.com>
2022-07-06 10:09:48 +0200

    Moved sorting settings menu to periods

--- src/app/period/period.component.ts
+++ src/app/period/period.component.ts
@@ -3,6 +3,7 @@ import {AppComponent} from "../app.component";
 import {Project} from "../project/project";
 import {Student} from "../student/student";
 import {Allocation} from "../allocation/allocation";
+import {OrderType} from "../project/order-type.enum";
 
 
 /**
@@ -43,6 +44,58 @@ export class PeriodComponent {
      */
     getAllocationsByStudentAndProject(student: Student, project: Project): Allocation[] {
         return this.appComponent.getAllocationsByProject(project).filter((allocation: Allocation) => allocation.student?.id === student.id);
+    }
+
+
+
+    /**
+     * Access to change order type in frontend
+     * @param begin = true; end = false
+     * @param desc = true; asc = false
+     * @param byName = true; byDate = false
+     */
+    orderStudentHelper(begin: boolean, desc: boolean, byName: boolean): void {
+        if (byName) {
+            this.appComponent.sortStudent = desc
+                ? OrderType.NAME_DESC
+                : OrderType.NAME_ASC;
+        } else if (begin) {
+            this.appComponent.sortStudent = desc
+                ? OrderType.BEGIN_DESC
+                : OrderType.BEGIN_ASC;
+        } else {
+            this.appComponent.sortStudent = desc
+                ? OrderType.END_DESC
+                : OrderType.END_ASC;
+        }
+    }
+
+    /**
+     * Adds ability to call sorting methods from html file (cannot call OrderTypes there)
+     * @param begin = true / end = false
+     * @param desc = true / asc = false
+     * @param byName = true / byDate = false
+     */
+    orderProjectHelper(begin: boolean, desc: boolean, byName: boolean): void {
+        if (byName) {
+            if (desc) {
+                this.appComponent.orderProject(OrderType.NAME_DESC);
+            } else {
+                this.appComponent.orderProject(OrderType.NAME_ASC);
+            }
+        } else if (begin) {
+            if (desc) {
+                this.appComponent.orderProject(OrderType.BEGIN_DESC);
+            } else {
+                this.appComponent.orderProject(OrderType.BEGIN_ASC);
+            }
+        } else {
+            if (desc) {
+                this.appComponent.orderProject(OrderType.END_DESC);
+            } else {
+                this.appComponent.orderProject(OrderType.END_ASC);
+            }
+        }
     }
 
 }
