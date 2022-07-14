07a1a85773a45d3fa47f3207ca470c2ddbc7b7cd
philipp <mail@philipp-bomers.de>
2022-07-14 13:08:48 +0200

    separated enum folder

---
+++ src/app/enum/order-type.enum.ts
@@ -0,0 +1,8 @@
+export enum OrderType {
+  BEGIN_DESC=0,
+  BEGIN_ASC=1,
+  END_DESC=2,
+  END_ASC=3,
+  NAME_DESC=4,
+  NAME_ASC=5
+}
--- src/app/project/order-type.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum OrderType {
-  BEGIN_DESC,
-  BEGIN_ASC,
-  END_DESC,
-  END_ASC,
-  NAME_DESC,
-  NAME_ASC
-}
